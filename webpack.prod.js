fa66e199b14ec4328810535c006e758470a4894d
agatalts <agata.e.latos@gmail.com>
2021-05-28 13:20:48 +0000

    add service workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@
 const path = require("path")
 const webpack = require("webpack")
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     output: {
@@ -24,6 +25,7 @@ module.exports = {
             ]
     },
     plugins: [
+    new WorkboxPlugin.GenerateSW(),
     new HtmlWebPackPlugin({
         template: "./src/client/views/index.html",
         filename: "./index.html",
